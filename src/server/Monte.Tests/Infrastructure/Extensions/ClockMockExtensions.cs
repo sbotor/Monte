b56e41dd2baf42bc34d93d3265b76345f8969105
Szymon <botorszym@gmail.com>
2023-12-13 22:07:34 +0100

    Add a few basic unit tests

---
+++ src/server/Monte.Tests/Infrastructure/Extensions/ClockMockExtensions.cs
@@ -0,0 +1,13 @@
+﻿using NSubstitute;
+
+namespace Monte.Tests.Infrastructure.Extensions;
+
+public static class ClockMockExtensions
+{
+    public static void Setup(this IClock clock, DateTime now)
+    {
+        clock.Now.Returns(now);
+        clock.UtcNow.Returns(now);
+        clock.Today.Returns(now.Date);
+    }
+}

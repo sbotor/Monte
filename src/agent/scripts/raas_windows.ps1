6a7ac8d3685fee3eb99cf2909b59c5d9f3bcca67
studentairu13 <dajurowski@gmail.com>
2024-01-28 17:27:05 +0100

    added powershell script for archiving reasons

---
+++ src/agent/scripts/raas_windows.ps1
@@ -0,0 +1,27 @@
+﻿$sourcePath = ".\\"
+$destinationPath = "$env:ProgramData\\Monte"
+
+if (!(Test-Path -Path $destinationPath)) {
+    New-Item -ItemType Directory -Path $destinationPath
+}
+
+Get-ChildItem -Path $sourcePath -Recurse | Copy-Item -Destination $destinationPath
+
+$nssmPath = "$destinationPath\\nssm.exe"
+$exePath = "$destinationPath\\main.exe" 
+
+$serviceName = "MonteAgent"
+
+& $nssmPath install $serviceName $exePath
+& $nssmPath set $serviceName AppParameters "--config production"
+& $nssmPath set $serviceName DisplayName "Agent for Monte"
+
+
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+if ($service.Status -ne 'Running') {
+    Start-Service -Name $serviceName -ErrorAction Stop
+    Write-Host "Service $serviceName started."
+} else {
+    Write-Host "Service $serviceName already started."
+}
